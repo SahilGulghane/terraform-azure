470a1d52609e90007152cd823cab72c4cfd2c91e
Sahil Gulghane <101868182+SahilGulghane@users.noreply.github.com>
2024-03-06 13:32:19 +0530

    Update code.tf

--- code.tf
+++ code.tf
@@ -9,19 +9,3 @@ resource "azurerm_resource_group" "rg" {
   location = "northeurope"
 }
 
-resource "azurerm_kubernetes_cluster" "cluster" {
-  name                = "learnk8scluster"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = "learnk8scluster"
-
-  default_node_pool {
-    name       = "default"
-    node_count = "2"
-    vm_size    = "Standard_B2s"
-  }
-
-  identity {
-    type = "SystemAssigned"
-  }
-}
