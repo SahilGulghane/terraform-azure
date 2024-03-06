1505a08393285fa0b3df9e097b039862ef52d49d
Sahil Gulghane <101868182+SahilGulghane@users.noreply.github.com>
2024-03-06 11:55:56 +0530

    Update code.tf

--- code.tf
+++ code.tf
@@ -1,61 +1,30 @@
 
+provider "azurerm" {
+  features {}
+    client_id      = "bcff84c6-e588-459f-9fda-39da37afb463"
+    client_secret  = "Cb58Q~BEqhYcUKfO1cJUrUlc6pAQpYrAJQdAFcuU"
+    tenant_id      = "e2c2296f-4b99-4a66-b84b-ce3fd333945a"
+    subscription_id= "f41f9a5f-78b9-4e1f-891f-2f748f1c64ed"
+}
 
-# Create virtual network
-resource "azurerm_virtual_network" "my_terraform_network" {
-  name                = "my-vnet"
-  address_space       = ["10.0.0.0/16"]
+resource "azurerm_resource_group" "rg" {
+  name     = "learnk8sResourceGroup"
+  location = "northeurope"
+}
+
+resource "azurerm_kubernetes_cluster" "cluster" {
+  name                = "learnk8scluster"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-}
+  dns_prefix          = "learnk8scluster"
 
-# Create subnets
-resource "azurerm_subnet" "my_terraform_subnet_a" {
-  name                 = "my-subnet-A"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.my_terraform_network.name
-  address_prefixes     = ["10.0.0.0/24"]
-}
-
-resource "azurerm_subnet" "my_terraform_subnet_b" {
-  name                 = "AzureBastionSubnet"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.my_terraform_network.name
-  address_prefixes     = ["10.0.1.0/28"]
-}
-
-# Create Windows Virtual Machine
-resource "azurerm_windows_virtual_machine" "my_windows_vm" {
-  name                  = "my-windows-vm"
-  resource_group_name   = azurerm_resource_group.rg.name
-  location              = azurerm_resource_group.rg.location
-  size                  = "Standard_DS2_v2"
-  admin_username        = "adminuser"
-  admin_password        = "Password123!"  # Replace with a secure password
-
-  network_interface_ids = [azurerm_network_interface.my_windows_vm_nic.id]
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
+  default_node_pool {
+    name       = "default"
+    node_count = "2"
+    vm_size    = "Standard_B2s"
   }
 
-  source_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2019-Datacenter"
-    version   = "latest"
-  }
-}
-
-# Create Network Interface for the Windows VM
-resource "azurerm_network_interface" "my_windows_vm_nic" {
-  name                      = "my-windows-vm-nic"
-  resource_group_name       = azurerm_resource_group.rg.name
-  location                  = azurerm_resource_group.rg.location
-
-  ip_configuration {
-    name                          = "my-windows-vm-ipconfig"
-    subnet_id                     = azurerm_subnet.my_terraform_subnet_a.id
-    private_ip_address_allocation = "Dynamic"
+  identity {
+    type = "SystemAssigned"
   }
 }
