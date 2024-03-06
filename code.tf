53a1cf05936a6d3a200cf4bb462a95a718e62e50
Sahil Gulghane <101868182+SahilGulghane@users.noreply.github.com>
2024-03-06 12:01:17 +0530

    Update code.tf
    removed secret

--- code.tf
+++ code.tf
@@ -1,10 +1,7 @@
 
 provider "azurerm" {
   features {}
-    client_id      = "bcff84c6-e588-459f-9fda-39da37afb463"
-    client_secret  = "Cb58Q~BEqhYcUKfO1cJUrUlc6pAQpYrAJQdAFcuU"
-    tenant_id      = "e2c2296f-4b99-4a66-b84b-ce3fd333945a"
-    subscription_id= "f41f9a5f-78b9-4e1f-891f-2f748f1c64ed"
+   
 }
 
 resource "azurerm_resource_group" "rg" {
