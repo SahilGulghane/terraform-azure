f6fb27e4d0ba5d5b5bf72f9b08107497c0f089b9
Sahil Gulghane <101868182+SahilGulghane@users.noreply.github.com>
2024-03-06 13:52:01 +0530

    Update code.tf
    addedd vnet subnet and vm

--- code.tf
+++ code.tf
@@ -13,3 +13,60 @@ resource "azurerm_resource_group" "rg2" {
   name     = "sahil"
   location = "northeurope"
 }
+
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "example" {
+  name                = "example-pip"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.example.id
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "example" {
+  name                  = "example-vm"
+  resource_group_name   = azurerm_resource_group.example.name
+  location              = azurerm_resource_group.example.location
+  network_interface_ids = [azurerm_network_interface.example.id]
+  size                  = "Standard_DS1_v2"
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-datacenter"
+    version   = "latest"
+  }
+
+  admin_username = "adminuser"
+  admin_password = "Password1234!"
+}
